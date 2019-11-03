54bc32afcf47c1f832b8b40c8e40a18b3ddebbde
FranciumEX1 <44691293+FranciumEX1@users.noreply.github.com>
2019-11-03 23:02:06 +0000

    Update footvis.py
    Visual change.

--- english premier league/footvis.py
+++ english premier league/footvis.py
@@ -7,65 +7,31 @@ from plotly.graph_objs import Bar
 from plotly.graph_objs import Layout
 from plotly.graph_objs import Figure
 
+LEAGUE_ROUND = 12
 
-leagueRound = 9
-
-fixtures = getFixtures(leagueRound)
-fullTable = getLeagueTable(leagueRound)
+fixtures = getFixtures(LEAGUE_ROUND)
+fullTable = getLeagueTable(LEAGUE_ROUND)
 allTeams = fullTable["Team"]
 fullWorstTable = getWorstPosition(fixtures, fullTable, allTeams)
 fullBestTable = getBestPosition(fixtures, fullTable, allTeams)
 
 fullWorstTable = fullWorstTable.sort_values("Position", ascending=False)
 fullBestTable = fullBestTable.sort_values("Position", ascending=False)
+finalTable = fullBestTable.merge(fullWorstTable, on="Team").sort_values("Position_x", ascending=False)
+finalTable["BestToWorst"] = finalTable["Position_y"]-finalTable["Position_x"]
 
 visualTablePlaces = Bar(
     y=fullTable["Team"],
-    x=fullTable["Position"],
+    x=[(position if position != 0 else 0.5) for position in finalTable["BestToWorst"]],
     orientation="h",
-    text=fullTable["Position"],
+    text=finalTable["Position_x"],
     textposition="inside",
     name="Current Position",
     insidetextanchor="start",
+    base=finalTable["Position_x"],
     width=[0.8 for team in fullTable["Team"]],
     marker=dict(
-        color="rgb(102, 168, 255)",
-        opacity=0.4,
-        line=dict(
-            color="rgb(92, 92, 92)", 
-            width=0.5
-        )
-    )
-)
-
-visualWorstPlaces = Bar(
-    y=fullTable["Team"],
-    x=fullWorstTable["Position"],
-    orientation="h",
-    text=fullWorstTable["Position"],
-    textposition="inside",
-    name="Worst Position",
-    width=[0.9 for team in fullWorstTable["Team"]],
-    marker=dict(
-        color="rgb(230, 38, 34)",
-        opacity=0.3,
-        line=dict(
-            color="rgb(92, 92, 92)", 
-            width=0.5
-        )
-    )
-)
-
-visualBestPlaces = Bar(
-    y=fullTable["Team"],
-    x=fullBestTable["Position"],
-    orientation="h",
-    text=fullBestTable["Position"],
-    textposition="inside",
-    name="Best Position",
-    width=[0.6 for team in fullBestTable["Team"]],
-    marker=dict(
-        color="rgb(136, 255, 0)",
+        color="rgb(226, 230, 4)",
         opacity=0.4,
         line=dict(
             color="rgb(92, 92, 92)", 
@@ -75,11 +41,12 @@ visualBestPlaces = Bar(
 )
 
 layout = Layout(
-    paper_bgcolor="rgb(255, 248, 196)",
-    plot_bgcolor="rgb(255, 248, 196)"
+    paper_bgcolor="rgb(56, 56, 56)",
+    plot_bgcolor="rgb(56, 56, 56)"
 )
  
-fullVisual = Figure(data=[visualTablePlaces, visualWorstPlaces, visualBestPlaces], layout=layout)
-fullVisual.update_layout(barmode='overlay', height=700, margin=dict(t=25))
-fullVisual.update_xaxes(nticks=20, showgrid=False)
+fullVisual = Figure(data=[visualTablePlaces], layout=layout)
+fullVisual.update_layout(barmode='overlay', height=700, margin=dict(t=25), xaxis=dict(tickmode='array', tickvals=fullTable["Position"]))
+fullVisual.update_xaxes(nticks=20, showgrid=False, tickfont=dict(family='Helvetica', color='yellow'))
+fullVisual.update_yaxes(tickfont=dict(family="Helvetica", size=18, color="yellow"))
 plotly.offline.plot(fullVisual)
