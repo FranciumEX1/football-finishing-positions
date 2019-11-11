9aaa16c0db15767ffc4ecf3100d963efe3a37c19
FranciumEX1 <44691293+FranciumEX1@users.noreply.github.com>
2019-11-11 05:05:59 +0000

    Update and rename footvis.py to visualisation.py

--- english premier league/visualisation.py
+++ english premier league/visualisation.py
@@ -5,11 +5,12 @@ from getLeagueData import getFixtures
 from bestFinish import getBestPosition
 from worstFinish import getWorstPosition
 from plotly.graph_objs import Bar
+from plotly.graph_objs import Scatter
 from plotly.graph_objs import Layout
 from plotly.graph_objs import Figure
 from teamcolours import getPLColours
 
-LEAGUE_ROUND = 12
+LEAGUE_ROUND = 13
 
 fixtures = getFixtures(LEAGUE_ROUND)
 fullTable = getLeagueTable(LEAGUE_ROUND)
@@ -24,13 +25,29 @@ finalTable = fullBestTable.merge(fullWorstTable, on="Team").sort_values("Positio
 finalTable = finalTable.merge(teamColourTable, on="Team")
 finalTable["BestToWorst"] = finalTable["Position_y"]-finalTable["Position_x"]
 
+visualPosition = Scatter(
+    y=fullTable["Team"],
+    x=fullTable["Position"],
+    mode="markers",
+    name="Current Position",
+    marker=dict(
+        size=16,
+        color=finalTable["Colour"],
+        opacity=0.7,
+        line=dict(
+            color="rgb(92, 92, 92)", 
+            width=0.5
+        )
+    )
+)
+
 visualTablePlaces = Bar(
     y=fullTable["Team"],
-    x=[(position if position != 0 else 0.5) for position in finalTable["BestToWorst"]],
+    x=[position for position in finalTable["BestToWorst"] if position != 0],
     orientation="h",
     #text=finalTable["Position_x"],
     textposition="inside",
-    name="Current Position",
+    name="Possible Positions",
     insidetextanchor="start",
     base=finalTable["Position_x"],
     width=[0.6 for team in fullTable["Team"]],
@@ -49,7 +66,7 @@ layout = Layout(
     plot_bgcolor="rgb(26, 26, 26)"
 )
  
-fullVisual = Figure(data=[visualTablePlaces], layout=layout)
+fullVisual = Figure(data=[visualTablePlaces, visualPosition], layout=layout)
 fullVisual.update_layout(barmode='overlay', height=700, margin=dict(t=25), xaxis=dict(tickmode='array', tickvals=fullTable["Position"]))
 fullVisual.update_xaxes(nticks=20, showgrid=True, tickfont=dict(family='Helvetica', color='yellow'), gridcolor="rgb(70, 70, 70)")
 fullVisual.update_yaxes(tickfont=dict(family="Helvetica", size=18, color="yellow"), showgrid=True, gridcolor="rgb(70, 70, 70)")
