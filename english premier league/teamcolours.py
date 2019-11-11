701c69051494e5d6791d53a444f42614567d2584
FranciumEX1 <44691293+FranciumEX1@users.noreply.github.com>
2019-11-11 05:07:29 +0000

    Update teamcolours.py

--- english premier league/teamcolours.py
+++ english premier league/teamcolours.py
@@ -10,7 +10,7 @@ def getPLColours():
 		"Team": ["Watford", "Norwich City", "Southampton", "Everton", "Aston Villa", "Newcastle United", "Burnley", "West Ham United",
 				"Wolverhampton Wanderers", "Tottenham Hotspur", "Manchester United", "Crystal Palace", "Brighton & Hove Albion", "Bournemouth",
 				"Sheffield United", "Arsenal", "Chelsea", "Leicester City", "Manchester City", "Liverpool"],
-		"Colour":["rgb(245, 245, 10)", "rgb(118, 227, 9)", "rgb(255, 92, 74)", "rgb(7, 86, 242)", "rgb(103, 14, 54)", "rgb(187, 188, 188)",
+		"Colour": ["rgb(245, 245, 10)", "rgb(118, 227, 9)", "rgb(255, 92, 74)", "rgb(7, 86, 242)", "rgb(103, 14, 54)", "rgb(187, 188, 188)",
 				"rgb(108, 29, 69)", "rgb(122, 38, 58)", "rgb(253, 185, 19)", "rgb(19, 34, 87)", "rgb(218, 41, 28)", "rgb(27, 69, 143)",
 				"rgb(27, 69, 143)", "rgb(218, 41, 28)", "rgb(227, 18, 2)", "rgb(230, 78, 67)", "rgb(52, 0, 242)", "rgb(81, 33, 255)",
 				"rgb(28, 255, 244)", "rgb(255, 13, 13)"]
